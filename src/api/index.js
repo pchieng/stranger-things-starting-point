774d3054e4f76120709ab4aa4a7d58d82b7aae46
Peter Chieng <pchieng3@gmail.com>
2022-03-09 21:08:15 -0600

    added GetPosts

--- src/api/index.js
+++ src/api/index.js
@@ -1,51 +1,60 @@
 export const getPosts = async () => {
     const url = 'https://strangers-things.herokuapp.com/api/2112-ftb-et-web-pt/posts';
     const response = await fetch(url)
-    const json = await response.json()
-    return json;
-} 
-
-export const createNewPost = async (newPost) => {
-    const url = 'https://jsonplaceholder.typicode.com/posts/';
-    const response = await fetch(url, {
-        method: "POST",
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer TOKEN_STRING_HERE'
-        },
-        body: JSON.stringify(newPost)
-    });
-
-    const json = await response.json();
-    console.log(json);
-    return json;
+        .then(response => response.json())
+        .then(result => {
+            console.log(result);
+            return result;
+        })
+        .catch(console.error);
+        
+        return response
+    // const json = await response.json();
+    // console.log(json);
+    // return json;
 };
 
-export const updateNewPost = async (newPost) => {
-    const url = 'https://jsonplaceholder.typicode.com/posts/';
-    const response = await fetch(url, {
-        method: "PATCH",
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(newPost)
-    });
+// export const createNewPost = async (newPost) => {
+//     const url = 'https://jsonplaceholder.typicode.com/posts/';
+//     const response = await fetch(url, {
+//         method: "POST",
+//         headers: {
+//             'Content-Type': 'application/json',
+//             'Authorization': 'Bearer TOKEN_STRING_HERE'
+//         },
+//         body: JSON.stringify(newPost)
+//     });
 
-    const json = await response.json();
-    console.log(json);
-    return json;
-};
+//     const json = await response.json();
+//     console.log(json);
+//     return json;
+// };
+
+// export const updateNewPost = async (newPost) => {
+//     const url = 'https://jsonplaceholder.typicode.com/posts/';
+//     const response = await fetch(url, {
+//         method: "PATCH",
+//         headers: {
+//             'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify(newPost)
+//     });
+
+//     const json = await response.json();
+//     console.log(json);
+//     return json;
+// };
 
 
 
 
 
-export const deletePostById = async (postId) => {
-    const url = `https://jsonplaceholder.typicode.com/posts/${postId}`;
-    const response = await fetch(url, {
-        method: "DELETE"
-    });
-    const json = await response.json();
-    console.log(json);
-    return json;
-};
+// export const deletePostById = async (postId) => {
+//     const url = `https://jsonplaceholder.typicode.com/posts/${postId}`;
+//     const response = await fetch(url, {
+//         method: "DELETE"
+//     });
+//     const json = await response.json();
+//     console.log(json);
+//     return json;
+// };
